e6c1afeaf51af07f9e1c300b8190702b8b6a9a7d
macjabeth <macbethjonathan@gmail.com>
2021-02-06 14:56:19 -0500

    👌 IMPROVE: html sanitization & body scraping

--- api/src/services/posts/posts.js
+++ api/src/services/posts/posts.js
@@ -1,19 +1,30 @@
 import fetch from 'node-fetch'
-import { markdown } from 'markdown'
+import cheerio from 'cheerio'
+import marked from 'marked'
+import sanitizeHtml from 'sanitize-html'
 
-const sanitize = (text) => {
-  let html = markdown.toHTML(text)
-  if (html.length > 255) html = html.slice(0, 252) + '...'
-  return html
+const fetchMetaDescription = async (url) => {
+  const res = await fetch(url)
+  const text = await res.text()
+  const $ = cheerio.load(text)
+  return $('meta[name="description"]').attr('content')
 }
 
-const cleanBody = (post) => ({
-  ...post,
-  body: post.body.startsWith('http') ? null : sanitize(post.body),
-})
+const truncate = (text) =>
+  text.length > 255 ? text.slice(0, 252) + '...' : text
+
+const cleanPost = async (post) => {
+  if (!post.body.startsWith('http')) {
+    post.body = truncate(sanitizeHtml(marked(post.body)))
+  } else {
+    post.body = await fetchMetaDescription(post.link_url)
+  }
+
+  return post
+}
 
 export const posts = async () => {
   const response = await fetch(process.env.GRIZZLY_SERVER)
   const data = await response.json()
-  return data.map(cleanBody)
+  return data.sort((a, b) => b.score - a.score).map(await cleanPost)
 }
